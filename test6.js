fa644d45a11b6d944b374d41bf9a691d62c5af83
donpat96 <ntsagonganbe.patrickdonatien@myu.universitecentrale.tn>
2019-10-08 15:33:49 +0100

    b

---
+++ test6.js
@@ -0,0 +1,6 @@
+var nod = document.getElementById("para");
+console.log("bonjour je suis le noeud " + nod.nodeName);
+console.log("Pour visualiser la difference entre firstChild et firstElementChild ");
+console.log("Bonjour je suis le first child : " + nod.firstChild);
+console.log("Bonjour je suis le firsst ElementChild : " + nod.firstElementChild);
+
